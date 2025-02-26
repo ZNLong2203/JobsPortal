161eaaff128892d09eac64a4a6d40596891941b2
Nguyễn Ngọc Long <51698746+ZNLong2203@users.noreply.github.com>
2025-02-26 14:50:00 +0700

    fix: register and login bugs

--- client/src/redux/api/authApi.ts
+++ client/src/redux/api/authApi.ts
@@ -11,16 +11,16 @@ interface AuthResponse {
 }
 
 export const registerApi = async (name: string, email: string, password: string): Promise<void> => {
-  await axiosInstance.post('/auth/register', { name, email, password })
+  await axiosInstance.post('/auth/register', { name, email, password }, { withCredentials: true })
 }
 
 export const loginApi = async (username: string, password: string): Promise<AuthResponse> => {
-  const response = await axiosInstance.post('/auth/login', { username, password })
+  const response = await axiosInstance.post('/auth/login', { username, password }, { withCredentials: true })
   return response.data.data
 }
 
 export const logoutApi = async (): Promise<void> => {
-  await axiosInstance.post('/auth/logout')
+  await axiosInstance.post('/auth/logout', { withCredentials: true })
 }
 
 export const loginWithGoogleApi = `${process.env.NEXT_PUBLIC_API_URL}/auth/google`
